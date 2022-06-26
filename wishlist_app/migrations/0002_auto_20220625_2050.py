739f4175fd09a279ab51f7e08b589a448ebcc6e2
1Reemab <Reemabdull0@gmail.com>
2022-06-27 01:36:32 +0300

    initial commit

--- wishlist_app/migrations/0002_auto_20220625_2050.py
+++ wishlist_app/migrations/0002_auto_20220625_2050.py
@@ -4,7 +4,7 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-    atomic = Falsecd ..
+    atomic = False
     dependencies = [
         ('wishlist_app', '0001_initial'),
     ]
